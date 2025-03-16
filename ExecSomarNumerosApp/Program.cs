0c1ef98540cf4e52db3be060f0088b4c4c515de0
Goesg <81311443+Goesg@users.noreply.github.com>
2025-03-16 23:20:48 +0000

    feat: first implementation project exercicio starting a new process

--- ExecSomarNumerosApp/Program.cs
+++ ExecSomarNumerosApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ExecSomarNumerosApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Init Projeto Somar Numeros");
+        }
+    }
+}
